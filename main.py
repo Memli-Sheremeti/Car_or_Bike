df55cff5c5ec2cdf00fc7af037802b0854a7b3dd
Memli-Sheremeti <memli.sh.pro@gmail.com>
2025-09-08 15:00:52 +0200

    Step 1 bis: Some data in different folder

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 #    By: mshereme <mshereme@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/09/08 13:20:26 by mshereme          #+#    #+#              #
-#    Updated: 2025/09/08 14:32:12 by mshereme         ###   ########.fr        #
+#    Updated: 2025/09/08 14:59:01 by mshereme         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -33,7 +33,7 @@ def image_to_test(dir, src_data):
         os.makedirs(dir, exist_ok=True)
         dataset = load_dataset(src_data)
         dataset_train = dataset["train"]
-        for i in range(5):  # save first 20 images
+        for i in range(200):  # save first 20 images
             img = dataset_train[i]["image"]
             img.save(f"{dir}/img_{i}.jpg")
     except Exception as e :
@@ -41,16 +41,19 @@ def image_to_test(dir, src_data):
     
 def main():
     try:
-        image_to_test(dir="data/car_images", src_data="fashxp/cars-manufacturers")
-        image_to_test(dir="data/bike_images", src_data="RuudVelo/my_awesome_new_bike")
+        image_to_test(dir="data/car", src_data="fashxp/cars-manufacturers")
+        image_to_test(dir="data/bike", src_data="Ketansomewhere/bikes")
     except Exception as e:
         print(f'Error as {e}')
-    # url = search_images("car", max_images=1)
-    # download_url(url[0], dest, show_progress=False)
-    # im = Image.open(dest)
-    # im.to_thumb(256, 256)
     
-    
+    dls = DataBlock(
+    blocks=(ImageBlock, CategoryBlock), 
+    get_items=get_image_files, 
+    splitter=RandomSplitter(valid_pct=0.2, seed=42),
+    get_y=parent_label,
+    item_tfms=[Resize(192, method='squish')]
+    ).dataloaders("data", bs=32)
+    dls.show_batch(max_n=6)
 
 
 if __name__ == "__main__":
